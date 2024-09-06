efa4bff39655c00a56b7450b34a5878479b25c39
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-05 22:04:20 -0500

    2

---
+++ Datos/Ejercicios/solicitudedatos.py
@@ -0,0 +1,7 @@
+nombreusuario = input("Ingrese su nonbre : ")
+edadusuario = int(input("ingrese su edad: "))
+
+print (f"Bienvenido su nombre es:{nombreusuario} y su edad es :{edadusuario}")
+
+edadusuario += 10
+print (f"{nombreusuario} Su edad En 10 años será: {edadusuario}")
