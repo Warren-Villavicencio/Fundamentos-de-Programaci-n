4fe6c32f8c99694ea276ff8d33c6bc8e647d8863
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-13 00:22:49 -0500

    h

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Nested if statements and elif statement/rollacoaster.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Nested if statements and elif statement/rollacoaster.py
@@ -4,12 +4,12 @@ height = int(input(" Ingresa a tu altura:"))
 
 
 if height >= 120:
-    
+    #mayor que 120
     print(" Puedes ingresar")
     age = int(input(" ¿Cuál es tu edad: "))
     if age <= 12:
         print(" Pagarás 5 dólares.")
-        
+        # menor que 12
     elif age <= 18:
     
         print(" Pagarás 7 dólares.")
