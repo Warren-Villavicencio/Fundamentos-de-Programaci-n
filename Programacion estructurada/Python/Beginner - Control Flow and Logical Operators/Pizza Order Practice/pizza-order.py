794cb1354103b31ac45d4b7451a73b562068176c
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-14 02:59:22 -0500

    i

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/pizza-order.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/pizza-order.py
@@ -6,5 +6,17 @@ add_peperroni = str(input("  Do you want pepperoni in your Pizza:  "))
 
 add_extra_chesse = str(input(" Do you want extra cheese. "))
 
-
+if size == "s":
+    pagaras = 5
+    print (" Pagarás 5 dólares") 
+elif size == "m":
+     pagaras = 9
+     print (" Pagarás 9 dólares")
+elif size == "l5":
+      pagaras = 15
+      print (" Pagarás 15 dólares")
+      
+      
+    
+    
 
