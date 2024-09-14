e85d9b79c9fa9752ea51f723d1f3cc03637ca4c4
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-14 02:54:21 -0500

    j

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/pizza-order.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/pizza-order.py
@@ -2,3 +2,8 @@ print(" Welcome to pizza order Delivery")
 
 size = str(input(" What size pizza do you want S, M or L: "))
 
+add_peperroni = str(input("  Do you want pepperoni in your Pizza:  "))
+
+add_extra_chesse = str(input(" Do you want extra cheese. "))
+
+
