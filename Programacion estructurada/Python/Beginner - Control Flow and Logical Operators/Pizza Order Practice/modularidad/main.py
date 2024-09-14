126a0c66580e25e532051d911141cada00ce3eb0
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-14 03:49:13 -0500

    j

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/modularidad/main.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/modularidad/main.py
@@ -1,22 +1,25 @@
-from funciones import get_pizza_size
-from funciones import get_pepperoni_choice
-from funciones import get_cheese_choice
-from funciones import calculate_price
+try:
+    from funciones import get_pizza_size, get_pepperoni_choice, get_cheese_choice, calculate_price
 
-# Programa principal
-print("Welcome to pizza order Delivery")
+    # Programa principal
+    print("Welcome to pizza order Delivery")
 
-# Obtener el tamaño de la pizza
-size = get_pizza_size()
+    # Obtener el tamaño de la pizza
+    size = get_pizza_size()
 
-# Obtener la elección de pepperoni
-add_pepperoni = get_pepperoni_choice()
+    # Obtener la elección de pepperoni
+    add_pepperoni = get_pepperoni_choice()
 
-# Obtener la elección de queso extra
-add_extra_cheese = get_cheese_choice()
+    # Obtener la elección de queso extra
+    add_extra_cheese = get_cheese_choice()
 
-# Calcular el precio final
-pagaras = calculate_price(size, add_pepperoni, add_extra_cheese)
+    # Calcular el precio final
+    pagaras = calculate_price(size, add_pepperoni, add_extra_cheese)
 
-# Mostrar el precio final
-print(f"Tu precio final es: {pagaras}")
+    # Mostrar el precio final
+    print(f"Tu precio final es: {pagaras}")
+
+except ImportError as e:
+    print(f"Error al importar: {e}")
+except Exception as e:
+    print(f"Ocurrió un error: {e}")
