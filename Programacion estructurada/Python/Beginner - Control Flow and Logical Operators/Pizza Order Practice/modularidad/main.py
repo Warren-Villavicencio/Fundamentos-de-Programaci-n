e83c844a425cdc22b66f054129ef11aae83c52fa
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-14 03:47:56 -0500

    m

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/modularidad/main.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Pizza Order Practice/modularidad/main.py
@@ -3,11 +3,20 @@ from funciones import get_pepperoni_choice
 from funciones import get_cheese_choice
 from funciones import calculate_price
 
-
-# Main program
+# Programa principal
 print("Welcome to pizza order Delivery")
+
+# Obtener el tamaño de la pizza
 size = get_pizza_size()
+
+# Obtener la elección de pepperoni
 add_pepperoni = get_pepperoni_choice()
+
+# Obtener la elección de queso extra
 add_extra_cheese = get_cheese_choice()
+
+# Calcular el precio final
 pagaras = calculate_price(size, add_pepperoni, add_extra_cheese)
+
+# Mostrar el precio final
 print(f"Tu precio final es: {pagaras}")
