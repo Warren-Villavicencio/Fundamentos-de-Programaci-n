2b1239f890f1cdb383e13cdd263f68936d1c2a99
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 17:04:31 -0500

    Auto-commit: Actualización de código

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Project Treasure Island/treasure-island.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Project Treasure Island/treasure-island.py
@@ -1,3 +1,3 @@
 print(" Welcome to the game")
 
-election_one = str(input(" Elige la derecha o la izquierda Inserta ¿Or para tu elección"))
+election_one = str(input(" Elige la derecha o la izquierda "))
