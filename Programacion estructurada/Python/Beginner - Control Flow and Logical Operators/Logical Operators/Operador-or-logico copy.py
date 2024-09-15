74b1fe346b0ac31c2be688606ca92fe0e6c65cf1
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 05:40:11 -0500

    Auto-commit: Actualización de código

---
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Logical Operators/Operador-or-logico copy.py
@@ -0,0 +1,9 @@
+# Ejemplo: Verificando si un caracter es una vocal
+# El operador 'or' solo necesita que una de las condiciones sea True.
+
+caracter = 'e'
+# Verificamos si el caracter es una vocal minúscula
+if caracter == 'a' or caracter == 'e' or caracter == 'i' or caracter == 'o' or caracter == 'u':
+    print("Es una vocal.")
+else:
+    print("No es una vocal.")
