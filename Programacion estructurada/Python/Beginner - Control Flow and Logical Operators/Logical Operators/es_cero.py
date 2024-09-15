1bf77b8e02b1ed18a72703e663e9f3ca928ed2e9
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 16:06:05 -0500

    k

---
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Logical Operators/es_cero.py
@@ -0,0 +1,12 @@
+# Archivo: es_cero.py
+"""
+Este programa verifica si un número es diferente de cero.
+"""
+
+numero = 0  # Cambia este valor para probar diferentes casos
+
+# Si el número no es cero, se imprime un mensaje
+if not numero == 0:
+    print("El número no es cero")
+else:
+    print("El número es cero")
