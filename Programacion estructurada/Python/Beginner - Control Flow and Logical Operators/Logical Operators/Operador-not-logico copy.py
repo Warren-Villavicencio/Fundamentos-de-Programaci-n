cc3bde1ea97ab086ea543636e043867a6c390846
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 05:41:43 -0500

    Auto-commit: Actualización de código

---
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Logical Operators/Operador-not-logico copy.py
@@ -0,0 +1,9 @@
+# Ejemplo: Verificando si un número es par
+# El operador 'not' invierte el valor de verdad de una condición.
+
+numero = 7
+# Verificamos si el número no es par (es decir, si es impar)
+if not numero % 2 == 0:
+    print("El número es impar.")
+else:
+    print("El número es par.")
