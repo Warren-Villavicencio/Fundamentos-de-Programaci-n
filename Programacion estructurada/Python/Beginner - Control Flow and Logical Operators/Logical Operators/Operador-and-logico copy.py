7e14e758250057b616105b306478dc1e0ea56825
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 05:43:05 -0500

    Auto-commit: Actualización de código

---
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Logical Operators/Operador-and-logico copy.py
@@ -0,0 +1,9 @@
+# Ejemplo: Verificando si un número está dentro de un rango
+# El operador 'and' asegura que ambas condiciones sean True.
+
+numero = 25
+# Verificamos si el número está entre 10 y 30 (inclusive)
+if 10 <= numero <= 30:
+    print("El número está dentro del rango.")
+else:
+    print("El número está fuera del rango.")
