938c7eca95564886ada78d06c2f8039c5a929079
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-09-15 17:17:19 -0500

    Auto-commit: Actualización de código

--- Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Project Treasure Island/treasure-island.py
+++ Programacion estructurada/Python/Beginner - Control Flow and Logical Operators/Project Treasure Island/treasure-island.py
@@ -17,7 +17,7 @@ elif election_one == "l":
             elif election_three == "y":
                 print(" you win ")
 else
-
+print(" Game over")
                 
               
               
